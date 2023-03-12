c641be3ccb55f97ef3def3a79778c5021fed12c3
Hafiz-Abdul-Basit <110740437+Hafiz-Abdul-Basit@users.noreply.github.com>
2023-03-12 14:31:44 +0500

    Database Initilaized  and added product and cateogry business entities

---
+++ PehliDukaan.Database/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace PehliDukaan.Database.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<PehliDukaan.Database.PDContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(PehliDukaan.Database.PDContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
---
+++ PehliDukaan.Database/PDContext.cs
@@ -0,0 +1,20 @@
+﻿using PehliDukaan.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PehliDukaan.Database {
+    public class PDContext : DbContext
+    {
+        public PDContext() : base("PehliDukaanConnection")
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+    }
+}
